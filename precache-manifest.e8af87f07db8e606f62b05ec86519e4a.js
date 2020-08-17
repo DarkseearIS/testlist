340287fc63835c22471c3ca0907d3d4fd20efb8b
Sergey Bocharov <da5229@yandex.ru>
2020-08-18 01:57:29 +0300

    Updates

---
+++ precache-manifest.e8af87f07db8e606f62b05ec86519e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4290e3d916c3cc142995e2d1feb6ac",
+    "url": "/testlist/index.html"
+  },
+  {
+    "revision": "8a32e3032e458ca59438",
+    "url": "/testlist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e404424e385c653d5c5",
+    "url": "/testlist/static/js/2.c3177dd4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/testlist/static/js/2.c3177dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a32e3032e458ca59438",
+    "url": "/testlist/static/js/main.88ceb069.chunk.js"
+  },
+  {
+    "revision": "4464d6bff4f4fde98450",
+    "url": "/testlist/static/js/runtime-main.66a17511.js"
+  }
+]);
